c0b168cde55eeb72a30a0dc27a93eff7fb48f11b
Estevão da Silva Florencio <126180457+Kyoda027@users.noreply.github.com>
2024-11-02 17:39:39 -0300

    Update insertionsort.c

--- Atividade_01/InsertionSort/insertionsort.c
+++ Atividade_01/InsertionSort/insertionsort.c
@@ -1,1 +1,36 @@
+//Aluno: Estevão
+#include <stdio.h>
+#define R 50
+
+void printArray(int arr[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int arr[], int tamanho) {
+    for (int i = 1; i < tamanho; i++) {
+        int teste = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > teste) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = teste;
+    }
+}
+
+int main() {
+    int arr[R] = {89, 12, 45, 67, 23, 10, 9, 35, 70, 56,
+                     78, 34, 25, 14, 90, 87, 65, 44, 13, 48,
+                     66, 28, 15, 3, 97, 54, 36, 27, 19, 41,
+                     64, 31, 26, 5, 43, 99, 75, 50, 6, 84,
+                     72, 16, 21, 33, 53, 18, 38, 24, 11, 77};
+                     
+    printf("Array ordenado:\n");
+    printArray(arr, R);
+
+    return 0;
+}
 
